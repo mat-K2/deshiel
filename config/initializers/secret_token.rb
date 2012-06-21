1ac6b9e1e108f99bb3659e29b0f0dde755953431
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2012-06-21 10:40:37 +0900

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Deshiel::Application.config.secret_token = '13ff6fab8308dacbaef3aa50c2c8f53b9d97a970e878c2a3adc74d2d37074cd267fd0a7d98b356da7a130474a7161c6c0e26f0b29f489af12452ea4a4046aa6b'
