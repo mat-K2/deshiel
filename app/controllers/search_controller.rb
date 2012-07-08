a3e8b084a844c3c7ae572834440b9fdb785bbd88
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2012-07-08 20:25:09 +0900

    Change view(search)

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,10 @@
 class SearchController < ApplicationController
   def index
+    @entry = current_user.entries.build
     if params[:search_type] == "entry"
       @entries = Entry.questions.select{ |entry| entry.content.include?(params[:entry_item])}
     elsif params[:search_type] == "master"
-      @masters = User.where("master_genre = ?", params[:master_genre])
+      @masters = User.where("id != ? AND master_genre = ?", current_user.id, params[:master_genre])
     end
   end
 
