3292a4be55685c6412fae30c15460f902a4a6c18
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2012-09-22 15:50:57 +0900

    Add filter(redirect to https)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :authenticate_user!
+  before_filter :redirect_https, :authenticate_user!
 
   include Jpmobile::ViewSelector
+
+  def redirect_https
+    if Rails.env == "production"
+      redirect_to :protocol => "https://" unless request.ssl?
+      return true
+    end
+  end
 end
