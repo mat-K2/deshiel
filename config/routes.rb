710864cb1e1514d579a498e712f6a0a5c5d852db
Keisuke Matsubara <k.matsu9@gmail.com>
2014-11-30 16:11:55 +0900

    add home view

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  root 'entries#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
