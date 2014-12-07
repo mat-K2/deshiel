759f227a96b82e368ebdaf75e6722521f6c8277d
Keisuke Matsubara <k.matsu9@gmail.com>
2014-12-07 22:20:14 +0900

    fix bug

--- app/controllers/pupil/entries_controller.rb
+++ app/controllers/pupil/entries_controller.rb
@@ -56,7 +56,7 @@ class Pupil::EntriesController < ApplicationController
   def destroy
     @entry.destroy
     respond_to do |format|
-      format.html { redirect_to entries_url, notice: 'Entry was successfully destroyed.' }
+      format.html { redirect_to pupil_home_path, notice: 'Entry was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
